e98bcf6d70cdd1f5b5dbf0399e59c90ca3fc693d
flaviogheri <109070685+flaviogheri@users.noreply.github.com>
2024-09-27 15:58:19 +0200

    Add Steak ConcreteBuilder

--- meal_builder.py
+++ meal_builder.py
@@ -1,17 +1,4 @@
-#!/usr/bin/env python3
-"""
-This code is a simple example of the Builder Design Pattern. Currently uses the builder method but doesnt take advantage
-(only one meal is available) - therefore dont have inherently different functions if I understand this correctly ?...
 
-Instead what uses effectively - Inheritance -> CheeseBurger etc.. inhereted other Burger types, of which add other topics
-
-Another effective tool -> Overriding functions -> In order to print the contents, 
-the classes that inhereted meal override it with their own elements.
-
-@Author: Flavio Gheri
-@Date: 26/09/24
-
-"""
 class Burger: 
 
     def __init__(self): 
@@ -22,30 +9,40 @@ class Burger:
     def list_parts(self): 
         return f"Meat: {self.meat_type}, Toppings: {', '.join(self.toppings)}, Cooking Level: {self.cooking_level}"
 
+class Steak:
+
+    def __init__(self):
+        self.meat_type = None 
+        self.cooking_level = None
+    
+    def list_parts(self):
+        return f"Steak: {self.meat_type} at {self.cooking_level}"
+
+
 
 class Meal: 
 
-    def __init__(self, burger, drink, side): 
+    def __init__(self, main, drink, side): 
 
-        self.burger = burger
+        self.main = main
         self.drink = drink
         self.side = side
 
     def list_parts(self): 
         # return f"Burger: {self.burger}, Drink: {self.drink}, Side: {self.side}"
-        return f"Burger with:  {self.burger.list_parts()}\nDrink: {self.drink}\nSide: {self.side}"
+        return f"Burger with:  {self.main.list_parts()}\nDrink: {self.drink}\nSide: {self.side}"
 
 
 
 class MealBuilder(): 
 
     def __init__(self): 
-        self.burger = None
+        self.main = None
         self.drink = None
         self.side = None
 
-    def set_burger(self, burger:Burger): 
-        self.burger = burger
+    def set_main(self, main): 
+        self.main = main
         return self
     
     def set_drink(self, drink): 
@@ -57,7 +54,7 @@ class MealBuilder():
         return self
     
     def build(self): 
-        return Meal(self.burger, self.drink, self.side)
+        return Meal(self.main, self.drink, self.side)
 
 
 class ClassicBurger(Burger): 
@@ -81,14 +78,30 @@ class AmericanBurger(CheeseBurger):
         super().__init__()
         self.toppings.extend(["bacon", "bbq sauce"])
 
+class WagyuSteak(Steak):
+
+    def __init__(self): 
+        super().__init__()
+        self.meat_type = "Wagyu"
+        self.cooking_level = "rare"
+
+
 
 if __name__=='__main__': 
 
     builder  = MealBuilder()
 
-    order = (builder.set_burger(AmericanBurger())
+    order = (builder.set_main(AmericanBurger())
                    .set_drink("soda")
                    .set_side("chips")
                    .build())
 
     print(order.list_parts())
+
+    order = (builder.set_main(WagyuSteak())
+                   .set_drink("soda")
+                   .set_side("chips")
+                   .build())
+
+    print(order.list_parts())
+
